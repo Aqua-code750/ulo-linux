be41eb33d2ba3cb071bda54fc7e9a88335d3349d
Aqua-code750 <Aqua-code750@users.noreply.github.com>
2026-07-05 11:53:40 +0530

    Fix: QEMU launch script on Windows when QEMU is not in PATH

--- launch-qemu.bat
+++ launch-qemu.bat
@@ -4,10 +4,15 @@ echo Launching Ulo Linux in QEMU...
 echo Make sure you have combined the ISO parts into 'ulo-linux.iso'
 if not exist ulo-linux.iso (
     echo [INFO] ulo-linux.iso not found! 
-    echo [INFO] Automatically merging downloaded parts (partaa, partab, partac)...
+    echo [INFO] Automatically merging downloaded parts partaa, partab, partac...
     copy /b ulo-linux.iso.part* ulo-linux.iso
     echo [INFO] Merge complete!
 )
 
-qemu-system-x86_64 -m 4G -smp 4 -cdrom ulo-linux.iso -vga virtio
+set QEMU_PATH=qemu-system-x86_64
+if exist "C:\Program Files\qemu\qemu-system-x86_64.exe" (
+    set QEMU_PATH="C:\Program Files\qemu\qemu-system-x86_64.exe"
+)
+
+%QEMU_PATH% -m 4G -smp 4 -cdrom ulo-linux.iso -vga virtio
 pause

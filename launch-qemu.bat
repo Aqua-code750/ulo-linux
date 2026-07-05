e9a059da1f89bc808ece65f6667a55c72966fd32
Aqua-code750 <Aqua-code750@users.noreply.github.com>
2026-07-05 12:53:46 +0530

    Fix: Change QEMU VGA driver to std to prevent blank display errors

--- launch-qemu.bat
+++ launch-qemu.bat
@@ -7,5 +7,5 @@ set QEMU_PATH=qemu-system-x86_64
 if exist "C:\Program Files\qemu\qemu-system-x86_64.exe" set QEMU_PATH="C:\Program Files\qemu\qemu-system-x86_64.exe"
 
 echo [INFO] Booting Ulo Linux Direct Kernel mode...
-%QEMU_PATH% -m 4G -smp 4 -kernel vmlinuz -initrd initrd -append "boot=casper quiet splash" -drive file=ulo-linux.iso,format=raw,media=cdrom -vga virtio
+%QEMU_PATH% -m 4G -smp 4 -kernel vmlinuz -initrd initrd -append "boot=casper quiet splash" -drive file=ulo-linux.iso,format=raw,media=cdrom -vga std
 pause

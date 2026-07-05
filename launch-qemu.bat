d9cd6bb88b3a2fdc212c57cd8b5cbd1878641409
Aqua-code750 <Aqua-code750@users.noreply.github.com>
2026-07-05 11:33:29 +0530

    Enhancement: Make launch script auto-merge split ISO parts

--- launch-qemu.bat
+++ launch-qemu.bat
@@ -2,6 +2,12 @@
 echo ==================================================
 echo Launching Ulo Linux in QEMU...
 echo Make sure you have combined the ISO parts into 'ulo-linux.iso'
-echo ==================================================
+if not exist ulo-linux.iso (
+    echo [INFO] ulo-linux.iso not found! 
+    echo [INFO] Automatically merging downloaded parts (partaa, partab, partac)...
+    copy /b ulo-linux.iso.part* ulo-linux.iso
+    echo [INFO] Merge complete!
+)
+
 qemu-system-x86_64 -m 4G -smp 4 -cdrom ulo-linux.iso -vga virtio
 pause
